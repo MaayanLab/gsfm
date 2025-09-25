4246d29b1050950f0661b566e68e2e7f1387f2e0
Daniel Clarke <u8sand@gmail.com>
2025-09-25 17:33:56 -0400

    Fetch/extract gene2go in python

--- gsfm/ablations/fetch_pmids.py
+++ gsfm/ablations/fetch_pmids.py
@@ -1,6 +1,14 @@
 import pandas as pd
+from gsfm import utils
 
-# curl -L https://ftp.ncbi.nih.gov/gene/DATA/gene2go.gz | gunzip > data/gene2go.tsv
+@utils.if_not_exists('data/gene2go.tsv')
+def _(file):
+  # curl -L https://ftp.ncbi.nih.gov/gene/DATA/gene2go.gz | gunzip > data/gene2go.tsv
+  import gzip, shutil
+  with gzip.open(utils.cached_wget('https://ftp.ncbi.nih.gov/gene/DATA/gene2go.gz'), 'rb') as fr:
+    with open(file, 'wb') as fw:
+      shutil.copyfileobj(fr, fw)
+
 gene2go = pd.read_csv('data/gene2go.tsv', sep='\t')
 gene2go['PubMed'] = gene2go['PubMed'].str.split('|')
 pd.Series(gene2go['PubMed'].explode().unique()).to_csv('data/pmids.txt', header=False, index=None)
